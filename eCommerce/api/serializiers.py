e903ee35c5436faf723c3fe3406cd95aec68055b
unknown <grigor.stoyanov@hotmail.com>
2022-08-22 02:14:13 +0300

    Added Monthly report generator api for total orders and total profit by custom date range

--- eCommerce/api/serializiers.py
+++ eCommerce/api/serializiers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, fields
 from eCommerce.api.models import Order
 from eCommerce.api.models import Product
 
@@ -18,3 +18,20 @@ class OrderSerializer(serializers.ModelSerializer):
         representation = super(OrderSerializer, self).to_representation(instance)
         representation['products'] = ProductSerializer(instance.products.all(), many=True).data
         return representation
+
+
+class ReportSerializer(serializers.Serializer):
+    month = serializers.DateField()
+    value = serializers.FloatField()
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if self.context['request'].query_params['metric'] == 'count':
+            representation['value'] = int(representation['value'])
+        return representation
+
+
+class ValidateQueryParamsForReport(serializers.Serializer):
+    date_start = fields.DateField(format='%Y%m%d')
+    date_end = fields.DateField(format='%Y%m%d')
+    metric = fields.ChoiceField(choices=['count', 'price'])
--- eCommerce/api/urls.py
+++ eCommerce/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from eCommerce.api.views.generic_api_views import GenerateReportApiView
 from eCommerce.api.views.order_api_views import OrderListApiView, OrderUpdateApiView
 from eCommerce.api.views.product_api_views import ProductListApiView, ProductUpdateApiView
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('products/', ProductListApiView.as_view(), name='get all products'),
     path('products/<int:pk>/', ProductUpdateApiView.as_view(), name='alter product'),
     path('orders/', OrderListApiView.as_view(), name='get all orders'),
-    path('orders/<int:pk>/', OrderUpdateApiView.as_view(), name='alter order')
+    path('orders/<int:pk>/', OrderUpdateApiView.as_view(), name='alter order'),
+    path('stats/', GenerateReportApiView.as_view(), name='generate report')
 ]
